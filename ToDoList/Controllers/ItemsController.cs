4e84015939e830fc53caeb78ac6c105374f8abef
Christine Frank <christine.braun13@gmail.com>
2019-10-14 14:42:15 -0700

    Update Item controller and applicable controller and  view to reflect db values

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,34 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
-  public class ItemsController : Controller
-  {
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
+    public class ItemsController : Controller
     {
-      Category category = Category.Find(categoryId);
-      return View(category);
-    }
+        private readonly ToDoListContext _db;
 
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
-    {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
-      return View(model);
-    }
+        public ItemsController(ToDoListContext db)
+        {
+            _db = db;
+        }
 
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
-    {
-      Item.ClearAll();
-      return View();
+        public ActionResult Index()
+        {
+            List<Item> model = _db.Items.ToList();
+            return View(model);
+        }
     }
-  }
 }
